5dbdffa92ff4ce39734c4dae073e7c4a77cfe606
mjcbfl <86734693+mjcbfl@users.noreply.github.com>
2021-07-08 09:37:25 -0230

    Commit 4

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -5,9 +5,37 @@ dotenv.config();
 
 import { APIGatewayEvent, Context } from 'aws-lambda'
 
+const expressReceiver = new ExpressReceiver({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  processBeforeResponse: true
+});
+
+const app = new App({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  token: `${process.env.SLACK_BOT_TOKEN}`,
+  receiver: expressReceiver
+});
+
+function parseRequestBody(stringBody: string | null) {
+  try {
+    return JSON.parse(stringBody ?? "");
+  } catch {
+    return undefined;
+  }
+}
+
 export async function handler (event: APIGatewayEvent, context: Context) {
+
+  const payload = parseRequestBody(event.body);
+
+  if(payload && payload.type && payload.type === 'url_verification') {
+    return {
+      statusCode: 200,
+      body: payload.challenge
+    };
+  }
+  
   return {
     statusCode: 200,
-    body: "Hello, World"
   };
 }
